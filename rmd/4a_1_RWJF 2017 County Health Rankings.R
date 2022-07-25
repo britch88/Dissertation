6e37a4292f7888df3eb77507c4a2e62df8ac00c3
Brit Henderson <henderson@rserver02.corpmdrc.org>
2022-07-25 06:36:07 -0400

    Created 2017 indicators from RWJ County Health Rankings data and saved to rds file

--- rmd/4a_1_RWJF 2017 County Health Rankings.R
+++ rmd/4a_1_RWJF 2017 County Health Rankings.R
@@ -165,10 +165,202 @@ l <- list(ak2017,al2017,az2017,ar2017,co2017,ct2017,de2017,fl2017,ga2017,hi2017,
 library(data.table)
 rwjmain2017 <- rbindlist(l,  fill=FALSE, idcol=NULL)
 
-rwjmain2017 <- do.call("rbind", l)
+library(tidyverse)
+rwjmain2017.2 <- rwjmain2017 %>% select(-contains("Z-Score"),-contains("95% CI"))
+
+rwjmain2017.keep <- rwjmain2017.2 %>% select(FIPS,State,County, 'PCP Rate','Income Ratio','% LBW','% Uninsured','Violent Crime Rate')
+
+
+############################# Repeat for supplemental data
+
+al2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Alabama Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ak2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Alaska Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+
+az2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Arizona Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ar2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Arkansas Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ca2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings California Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+co2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Colorado Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ct2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Connecticut Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+de2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Delaware Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+dc2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings District of Columbia Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+fl2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Florida Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ga2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Georgia Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+hi2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Hawaii Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+id2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Idaho Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+il2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Illinois Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ind2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Indiana Data - v2.xls", 
+                      sheet = "Additional Measure Data", skip = 1)
+
+ia2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Iowa Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ks2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Kansas Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+kt2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Kentucky Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+la2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Louisiana Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+me2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Maine Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+md2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Maryland Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ma2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Massachusetts Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+mi2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Michigan Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+mn2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Minnesota Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ms2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Mississippi Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+mo2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Missouri Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+mt2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Montana Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ne2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Nebraska Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+nv2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Nevada Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+nh2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings New Hampshire Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+nj2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings New Jersey Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+nm2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings New Mexico Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ny2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings New York Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+nc2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings North Carolina Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+nd2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings North Dakota Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+oh2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Ohio Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ok2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Oklahoma Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+or2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Oregon Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+pa2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Pennsylvania Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ri2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Rhode Island Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+sc2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings South Carolina Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+sd2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings South Dakota Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+tn2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Tennessee Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+tx2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Texas Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+ut2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Utah Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+vt2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Vermont Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+va2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Virginia Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+wa2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Washington Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+wv2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings West Virginia Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+wi2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Wisconsin Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
+
+wy2017 <- read_excel("/data/share/xproject/training/Practice/henderson/Dissertation/raw/CountyPredictors/RWJF County Health Rankings 2017/2017 County Health Rankings Wyoming Data - v2.xls", 
+                     sheet = "Additional Measure Data", skip = 1)
 
 
 
+# Combined all the supplemental measures
+l <- list(ak2017,al2017,az2017,ar2017,co2017,ct2017,de2017,fl2017,ga2017,hi2017,
+          id2017,ind2017,ia2017,il2017,kt2017,ks2017,la2017,mn2017,mi2017,ms2017,
+          me2017,mo2017,ma2017,mt2017,nv2017,ne2017,nm2017,ny2017,nj2017,nc2017,
+          nd2017,oh2017,pa2017,ri2017,sc2017,sd2017,tx2017,tn2017,ok2017,ut2017,
+          vt2017,wa2017,wv2017,wi2017,wy2017,or2017,nh2017,va2017,ca2017,md2017,dc2017)
+
+rwjsupp2017 <- rbindlist(l,  fill=FALSE, idcol=NULL)
+rwjsupp2017.2 <- rwjsupp2017 %>% select(-contains("95% CI"))
+
+rwjsupp2017.keep <- rwjsupp2017.2 %>% select(FIPS,State,County, 
+                                             '% Disconnected Youth',
+                                             'Segregation index',
+                                             'Segregation Index',
+                                             '% < 18',
+                                             '% 65 and over',
+                                             '% African American',
+                                             '% American Indian/Alaskan Native',
+                                             '% Native Hawaiian/Other Pacific Islander',
+                                             '% Hispanic',
+                                             '% Non-Hispanic White',
+                                             '% Not Proficient in English',
+                                             '% Female',
+                                             '% Rural'
+                                             )
 
 
+####### Combine Main Data and Supplemental Data
+rwj2017.final <- merge(rwjmain2017.keep,rwjsupp2017.keep, by = c('FIPS','State','County'))
+
+
+
+###### Save the Data
+saveRDS(rwj2017.final, here::here("rda","rwj2017.rds"))
 
